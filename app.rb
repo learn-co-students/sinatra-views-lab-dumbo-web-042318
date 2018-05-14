18205608c6e00c3b041b5c7ebd3a650bc8f61ab8
github username <github email address>
2018-05-14 15:18:08 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,25 @@
+require_relative 'config/environment'
+
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
 
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end
+
+	get '/date' do
+		real_time = Time.now
+		# binding.pry
+		@time = real_time.strftime("The date is %A, %B %d, %Y")
+		# binding.pry
+		erb :date
+	end
 end
